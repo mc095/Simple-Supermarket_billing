aaa553d4fe33c45277b37fff9c8228e3905c2ed9
Ganesh Vathumilli <143707195+zcat07@users.noreply.github.com>
2023-11-13 07:55:06 +0530

    Update img-cd.py
    Updating only number recognition from each row and Placing a While loop for adding a new bill

--- img-cd.py
+++ img-cd.py
@@ -10,33 +10,42 @@ def extraction_and_accumulate_numbers(image_path):
     # Processing the result to extract whole numbers in row-wise
     accumulated_numbers = []
     current_row_numbers = []
+
     for detection in result:
         text = detection[1]
 
-        # Checking if the text is not empty or just a whitespaces
+        # Checking if the text is not empty or just whitespace
         if text.strip():
+            # If the text is a digit, converting it to an integer
             if text.isdigit():
-                # If the text is a digit, converting it to an integer
                 current_number = int(text)
                 # Adding the current number to the current row
                 current_row_numbers.append(current_number)
-                # Printing a message indicating the number being added to the current row
-                print("Adding rupees {} to the current list".format(current_number))
+            # If the text is not a digit and the current row has numbers
             elif current_row_numbers:
-                # If the current row has numbers, extending the accumulated list with the numbers from the current row
+                # Extending the accumulated list with the numbers from the current row
                 accumulated_numbers.extend(current_row_numbers)
                 # Resetting the current row for the next set of numbers
                 current_row_numbers = []
 
     # Handling the last row of numbers
     if current_row_numbers:
-        # If there are numbers in the last row, extending the accumulated list with those numbers
+        # Extending the accumulated list with the numbers from the last row
         accumulated_numbers.extend(current_row_numbers)
 
-    # Printing the accumulated numbers row-wise and the total sum
+    # Printing the accumulated numbers and the total sum
     print("\nPrices of Items as a list: {}".format(accumulated_numbers))
     print("\nTotal Bill of the Purchase: {}".format(sum(accumulated_numbers)))
 
-# Inserting the Image path from the system file
-image_path = input("Enter the path to your image file (with a .jpg extension): ")
-extraction_and_accumulate_numbers(image_path)
+# Main loop
+while True:
+    # Inserting the Image path from the user
+    image_path = input("Enter the path to your image file (with a .jpg or .png extension): ")
+    
+    # Processing the bill
+    extraction_and_accumulate_numbers(image_path)
+
+    # Asking the user if they want to enter another bill
+    another_bill = input("Do you want to enter another bill? (yes/no): ").lower()
+    if another_bill != 'yes':
+        break
